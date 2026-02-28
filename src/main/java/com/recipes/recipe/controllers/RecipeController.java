ef65ecbf2210fb806f7823f6fb62ce38d19f5f1e
yhaddar <youssefhaddar9@gmail.com>
2026-02-28 16:40:57 +0000

    fix bugs

--- src/main/java/com/recipes/recipe/controllers/RecipeController.java
+++ src/main/java/com/recipes/recipe/controllers/RecipeController.java
@@ -4,8 +4,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.concurrent.CompletableFuture;
-
 @RestController
 @RequestMapping("api/recipe")
 public class RecipeController {
