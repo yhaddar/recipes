0349bcc724c650192647cc1a07dc6f88c24adf10
yhaddar <youssefhaddar9@gmail.com>
2026-02-28 16:52:51 +0000

    edit the message of response

--- src/main/java/com/recipes/recipe/controllers/RecipeController.java
+++ src/main/java/com/recipes/recipe/controllers/RecipeController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class RecipeController {
     @GetMapping("")
     String index(){
-        return "hello world";
+        return "hello from recipe API";
     }
 }
