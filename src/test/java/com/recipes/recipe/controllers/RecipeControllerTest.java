5f5b6cfdbc15a94f4f15b8a76558c94476314d7f
yhaddar <youssefhaddar9@gmail.com>
2026-03-01 12:03:17 +0000

    start the testing for recipe(continue later)

---
+++ src/test/java/com/recipes/recipe/controllers/RecipeControllerTest.java
@@ -0,0 +1,22 @@
+package com.recipes.recipe.controllers;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RecipeControllerTest {
+
+    @Test
+    @DisplayName("method for test index function in recipe controller")
+    void testIndex(){
+        //
+    }
+
+    @Test
+    @DisplayName("method for test store function in recipe controller")
+    void testStore(){
+        //
+    }
+
+}
